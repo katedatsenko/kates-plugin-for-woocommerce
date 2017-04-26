3611bb2682e361faaf3ec792dfa029913bcda7b9
Kate Datsenko <kate.datsenko@gmail.com>
2017-04-26 22:23:53 +0300

    Plugin file

---
+++ kates-plugin-for-woccommerce.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Kate's Plugin For Woocommerce
+Plugin URI: https://github.com/katedatsenko/kates-plugin-for-woocommerce
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Kate Datsenko
+Author URI: https://github.com/katedatsenko/
+License: A "Slug" license name e.g. GPL2
+*/
